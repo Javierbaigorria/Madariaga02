a9c920fc57ee3c8d22bad0ad1564634bf33cc4ff
Javierbaigorria <92064095+Javierbaigorria@users.noreply.github.com>
2021-10-06 21:35:21 -0300

    Agregue carita una carita triste

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 console.log("Qué capo, ya estoy usando github y voy  hacer mi primer commit :)";
-console.log("estoy re perdido con GitHub");
+console.log("estoy re perdido con GitHub :(");
 console.log("ahi va queriendo dale");
